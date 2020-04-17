f91521a7b22a8d2367cf9b9b5d6735df666407ae
Klassare <klassare@gmail.com>
2020-04-18 01:07:15 +0200

    update crypto-utils

---
+++ packages/crypto-utils/src/common.test.ts
@@ -0,0 +1,25 @@
+import {
+  base58encode,
+  base58decode,
+  mergebuf,
+  hex2buf,
+  hex2pk,
+  pk2pkh,
+  buf2hex
+} from './common';
+
+describe('#hex2buf', () => {
+  it('should return buf', () => {
+    const buf = hex2buf('01af');
+    expect(buf).toBeDefined();
+    expect(JSON.stringify(buf)).toBe(JSON.stringify(Uint8Array.from([1, 175])));
+  });
+});
+
+describe('#buf2hex', () => {
+  it('should return buf', () => {
+    const hex = buf2hex(Uint8Array.from([1, 175]));
+    expect(hex).toBeDefined();
+    expect(hex.toString()).toBe('01af');
+  });
+});
